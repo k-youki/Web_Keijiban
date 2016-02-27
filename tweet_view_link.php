348badedb7edd14e05aaebddf8df9e30effbf8db
YUKI KOTANI <mocoruco+git@gmail.com>
2016-02-27 18:26:52 +0900

    インデント整理

--- tweet_view_link.php
+++ tweet_view_link.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,31 +22,29 @@
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
+</head>
 
-  <body>
+<body>
 
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="tweet.php">こたに掲示板</a>
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="tweet.php">こたに掲示板</a>
+            </div>
         </div>
-
-      </div>
     </nav>
 
-        <div class="col-md-3">
-
-          <form action ="tweet_ins.php" method="GET">
+    <div class="col-md-3">
+        <form action ="tweet_ins.php" method="GET">
             <br>
             お名前を入力してください。<br>
             <textarea name="account" cols="10" rows="1"></textarea>
@@ -57,56 +55,53 @@
             <br>
 
             <input type="submit" value="投稿" class="btn btn-primary" >
-          </form>
-        </div>
+        </form>
+    </div>
 
-        <div class="col-md-9">
-
-          <div class="table-responsive">
+    <div class="col-md-9">
+        <div class="table-responsive">
             <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>名前</th>
-                  <th>投稿内容</th>
-                  <th>投稿時間</th>
-                  <th>削除</th>
-                </tr>
-              </thead>
-              <tbody>
-<?php
-$connect = mysql_connect("localhost","root","");
+                <thead>
+                    <tr>
+                        <th>名前</th>
+                        <th>投稿内容</th>
+                        <th>投稿時間</th>
+                        <th>削除</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $connect = mysql_connect("localhost","root","");
 
-//SQLをUTF8形式で書くよ、という意味
-mysql_query("SET NAMES utf8",$connect);
+                    //SQLをUTF8形式で書くよ、という意味
+                    mysql_query("SET NAMES utf8",$connect);
 
-//登録された時間の新しい時間に並べて表示したい
-//この１行で実行
-$rs = mysql_db_query("test","select * from tweet_tbl order by input_datetime desc");
+                    //登録された時間の新しい時間に並べて表示したい
+                    //この１行で実行
+                    $rs = mysql_db_query("test","select * from tweet_tbl order by input_datetime desc");
 
-while(true){
-  $row = mysql_fetch_assoc($rs);
-  if($row == null){
-    break;
-  }else{
-    echo "<tr>";
-    echo "<td>{$row['account']}</td>";
-    echo "<td>{$row['contents']}</td>";
-    echo "<td>{$row['input_datetime']}</td>";
-    $tweet_id = $row["tweet_id"];
-    echo "<td><a href='tweet_del.php?tweet_id=$tweet_id'>削除</a></td>";
-    echo "</tr>";
-  }
-}
+                    while(true){
+                        $row = mysql_fetch_assoc($rs);
+                        if($row == null){
+                            break;
+                        }else{
+                            echo "<tr>";
+                            echo "<td>{$row['account']}</td>";
+                            echo "<td>{$row['contents']}</td>";
+                            echo "<td>{$row['input_datetime']}</td>";
+                            $tweet_id = $row["tweet_id"];
+                            echo "<td><a href='tweet_del.php?tweet_id=$tweet_id'>削除</a></td>";
+                            echo "</tr>";
+                        }
+                    }
 
-//データベースとの接続を切る
-mysql_close($connect);
-
-
-?>
-              </tbody>
+                    //データベースとの接続を切る
+                    mysql_close($connect);
+                    ?>
+                </tbody>
             </table>
-          </div>
         </div>
+    </div>
 
 
     <!-- Bootstrap core JavaScript
@@ -117,5 +112,5 @@ mysql_close($connect);
     <script src="css/assets/js/docs.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="css/assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+</body>
 </html>
